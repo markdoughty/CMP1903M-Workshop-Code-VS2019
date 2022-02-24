bbe500e5965ed13f8514e87feec97346a345c6a7
Computing <Computing@1A-F1-WINDOWS>
2022-02-24 12:35:57 +0000

    Add project files.

---
+++ CMP1903M Workshop Code VS2019/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Workshop_Code_VS2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Problem 1
+            //(-4, -5) and (-1, -1) describe two points. What is the distance between them?
+            int x1 = -4;
+            int y1 = -4;
+
+            int x2 = -1;
+            int y2 = -1;
+
+            int answer1 = 0;
+
+            //Problem 2
+            //What is the denominator of the slope between these points?
+
+
+            //Problem 3
+            //What is the greatest common divisor (GCD) of 1124 and 136?
+
+
+            //Problem 4
+            //What is the remainder, when you divide the square of the sum of the first ten natural numbers by ten?
+
+
+            Console.ReadKey();
+        }   
+     
+    }
+}
