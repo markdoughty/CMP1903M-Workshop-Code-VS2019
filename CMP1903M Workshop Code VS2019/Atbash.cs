6c804748330dae2637f068c74913bed2bb278f9f
Mark Doughty <mdoughty@lincoln.ac.uk>
2022-03-10 12:45:06 +0000

    Week 3

---
+++ CMP1903M Workshop Code VS2019/Atbash.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Workshop_Code_VS2019
+{
+    class Atbash
+    {
+        public string encode(string text)
+        {
+            return "";
+        }
+
+        public string decode(string text)
+        {
+            return "";
+        }
+    }
+}
