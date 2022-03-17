3602188123cf9c92b38e525bfd0083a91b6d990d
Mark Doughty <mdoughty@lincoln.ac.uk>
2022-03-17 12:55:32 +0000

    Week 4

---
+++ CMP1903M Workshop Code VS2019/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Workshop_Code_VS2019
+{
+    class Person
+    {
+        //fields
+        private string _firstName;
+        private string _secondName;
+        private string _email;
+
+        //properties
+
+
+        //constructors
+
+
+        //methods
+    }
+}
