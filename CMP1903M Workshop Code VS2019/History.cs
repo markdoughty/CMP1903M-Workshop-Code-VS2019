6d7cabc7abe588cbcaf50dcfaa271f9788c02d7c
Mark Doughty <mdoughty@lincoln.ac.uk>
2022-03-24 11:21:36 +0000

    Week 5

---
+++ CMP1903M Workshop Code VS2019/History.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Workshop_Code_VS2019
+{
+    class History
+    {
+        private Stack<Page> history;
+
+        //constructor
+        public History()
+        {
+            history = new Stack<Page>();
+        }
+    }
+}
